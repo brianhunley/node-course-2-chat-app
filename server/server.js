4bbb13116db6232f1091bb99ca0ed6ca6b15b42e
Brian Hunley <brian@hunleyconsulting.com>
2018-01-21 20:31:47 -0500

    Updated package node version for heroku

--- server/server.js
+++ server/server.js
@@ -7,6 +7,5 @@ const app = express();
 
 app.use(express.static(publicPath));
 app.listen(port, () => {
-  console.log(`Server is up on port ${port}`);
-})
-
+  console.log(`Server is running on port ${port}`);
+});
