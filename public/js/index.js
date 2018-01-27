5dc369fe816d36cc7e7e5e1d28b0e000b552fef0
Brian Hunley <brian@hunleyconsulting.com>
2018-01-27 06:55:36 -0500

    Add mustache.js for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -10,23 +10,29 @@ socket.on('disconnect', function() {
 
 socket.on('newMessage', function(message) {
     const formattedTime = moment(message.createdAt).format('h:mm a');
+    var template = $('#message-template').html();
+    // Mustache.parse(template);  // optional, speeds up future uses
+    var html = Mustache.render(template, {
+        from: message.from,
+        text: message.text,
+        createdAt: formattedTime
+    });
 
-    const li = $('<li></li>');
-    li.text(`${message.from} ${formattedTime}: ${message.text}`);
-
-    $('#messages').append(li);
+    $('#messages').append(html);
 });
 
 socket.on('newLocationMessage', function(message) {
     const formattedTime = moment(message.createdAt).format('h:mm a');
 
-    const li = $('<li></li>');
-    const a = $('<a target="_blank">My Current Location</a>');
-    li.text(`${message.from} ${formattedTime}: `);
-    a.attr('href', message.url);
-    li.append(a);
-  
-    $('#messages').append(li);    
+    var template = $('#location-message-template').html();
+    // Mustache.parse(template);  // optional, speeds up future uses
+    var html = Mustache.render(template, {
+        from: message.from,
+        url: message.url,
+        createdAt: formattedTime
+    });
+
+    $('#messages').append(html);    
 });
 
 $('#message-form').on('submit', function(e) {
